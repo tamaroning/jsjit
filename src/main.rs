21313b0881b42a1c56835c66fce6e653d6ede2b9
Raiki Tamura <tamaron1203@gmail.com>
2024-01-02 00:56:18 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::fs;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct JsonTopLevel {
+    functions: Vec<JsonFunction>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct JsonFunction {
+    name: String,
+    bytecode: Vec<u8>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<_> = std::env::args().collect();
+
+    let contents = fs::read_to_string(&args[1]).expect("Could not read file");
+    let json: JsonTopLevel = serde_json::from_str(&contents).unwrap();
+
+    dbg!(json);
 }
